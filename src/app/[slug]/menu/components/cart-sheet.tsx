bdaf396f67a4cdc54cfe4ed3585d3ee9804f568c
João Antônio de Castro <joaoaccastro@gmail.com>
2025-02-23 16:58:46 -0300

    feat: ensure cart is shown

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -1,31 +1,43 @@
 import { useContext } from "react";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import { formatCurrency } from "@/helpers/format-currency";
 
 import { CartContext } from "../contexts/cart";
 import CarProductItem from "./cart-product-item";
 
 const CartSheet = () => {
-  const { isOpen, toggleCart, products } = useContext(CartContext);
+  const { isOpen, toggleCart, products, total} = useContext(CartContext);
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
-      <SheetContent className="w-[80%]">
+      <SheetContent className="w-[90%]">
         <SheetHeader>
           <SheetTitle className="mb-2 border-b-2 border-b-red-400 pb-4 text-center">
             CARRINHO
           </SheetTitle>
-          <SheetDescription className="pb-6">Seus itens:</SheetDescription>
         </SheetHeader>
-        <div className="py-5">
-          {products.map((product) => (
-            <CarProductItem key={product.id} product={product} />
-          ))}
+        <div className="flex h-[95%] flex-col py-5">
+          <div className="flex-auto">
+            {products.map((product) => (
+              <CarProductItem key={product.id} product={product} />
+            ))}
+          </div>
+          <Card className="mb-6">
+            <CardContent className="p-5">
+              <div className="flex justify-between">
+                <p className="text-sm text-muted-foreground">Total</p>
+                <p className="text-sm font-semibold">{formatCurrency(total)}</p>
+              </div>
+            </CardContent>
+          </Card>
+          <Button className="w-full rounded-full">Finalizar Pedido</Button>
         </div>
       </SheetContent>
     </Sheet>
